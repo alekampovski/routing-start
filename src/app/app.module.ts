43bc576752e738c22b6fd1c0ba6e3f08a42d0139
alekampovski98 <alek.ampovski@intertec.io>
2022-10-15 12:01:28 +0200

    master: Implement routing module and wildcard route

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -13,7 +13,6 @@ import { ServerComponent } from './servers/server/server.component';
 import { ServersService } from './servers/servers.service';
 import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
 import {AppRoutingModule} from './app-routing.module';
-
 @NgModule({
   declarations: [
     AppComponent,
